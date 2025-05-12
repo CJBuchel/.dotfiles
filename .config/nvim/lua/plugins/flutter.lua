fd00b474387790afa21ffc5fb6d2742482c5798f
CJBuchel <connorbuchel@gmail.com>
2025-05-12 19:32:47 +0800

    Updated nvim

---
+++ .config/nvim/lua/plugins/flutter.lua
@@ -0,0 +1,13 @@
+return {
+  -- Flutter tools
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim',
+    },
+    config = true,
+  }
+
+}
